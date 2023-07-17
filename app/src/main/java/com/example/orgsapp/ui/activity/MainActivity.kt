20369bb2a2248e23062cb05dd62eb1a3793cfcca
EclipseBSD <eclipsebsdcontact@protonmail.com>
2023-07-17 16:23:59 -0400

    Implementando formulário de cadastro

--- app/src/main/java/com/example/orgsapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/orgsapp/ui/activity/MainActivity.kt
@@ -2,16 +2,16 @@ package com.example.orgsapp.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.orgsapp.R
 import com.example.orgsapp.model.Produtos
 import com.example.orgsapp.ui.recyclerview.adapter.ListaProdutosAdapter
 import java.math.BigDecimal
 
-class MainActivity : Activity(){
+class MainActivity : AppCompatActivity(R.layout.activity_main){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 //        val nome = findViewById<TextView>(R.id.nome)
 //        nome.text = "Cesta de Frutas"
 //        val descricao = findViewById<TextView>(R.id.descricao)

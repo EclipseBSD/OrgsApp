9850397082b2b8c37850efba0ffae5af79995a8e
EclipseBSD <eclipsebsdcontact@protonmail.com>
2023-07-25 14:33:34 -0400

    Personalizando o layout com fontes e tamanhos novos

--- app/src/main/java/com/example/orgsapp/dao/ProdutoDao.kt
+++ app/src/main/java/com/example/orgsapp/dao/ProdutoDao.kt
@@ -1,19 +1,26 @@
 package com.example.orgsapp.dao
 
 import com.example.orgsapp.model.Produtos
+import java.math.BigDecimal
 
 class ProdutoDao {
 
-    fun add(produto: Produtos){
+    fun add(produto: Produtos) {
         produtos.add(produto)
     }
 
-    fun buscaTodos() : List<Produtos>{
+    fun buscaTodos(): List<Produtos> {
         return produtos.toList()
     }
 
     companion object {
-        private val produtos = mutableListOf<Produtos>()
+        private val produtos = mutableListOf<Produtos>(
+            Produtos(
+                nome = "Salada de Frutas",
+                descricao = "Maçãs, Laranjas e diversas frutas",
+                preco = BigDecimal("19.99")
+            )
+        )
     }
 
 }
